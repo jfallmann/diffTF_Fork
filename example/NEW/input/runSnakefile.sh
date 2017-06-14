8e13d4003420fb631c5c01357a2cc156dd3ded57
Christian Arnold <christian.arnold@embl.de>
2017-06-14 14:21:21 +0200

    added example from CLL as orientation how to run it

---
+++ example/NEW/input/runSnakefile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+########################
+# PATHS AND PARAMETERS #
+########################
+
+# All parameters and paths are defined here:
+. "./params.sh"
+
+
+########################
+# RUN AUTOMATED SCRIPT #
+########################
+. "/g/scb/zaugg/zaugg_shared/scripts/Christian/src/Snakemake/runSnakemakeWrapper.sh"
